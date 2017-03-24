3b43a653801d01caec79ba3110dcdae8590d7fd8
Sepohobbs <seppohobbs@gmail.com>
2017-03-24 12:42:32 -0500

    3/24

--- InventoryMGMT/ITEM_CLASS.vb
+++ InventoryMGMT/ITEM_CLASS.vb
@@ -4,8 +4,18 @@
     Public price As Double
     Public stock As Integer
 
-    'Public Sub New(ByVal item_number As String, ByVal price As Double, ByVal stock As Integer)
-    '    item_number = item_num
-    '    price = price
+    Public Sub New(ByVal item_number As String, ByVal price As Double, ByVal stock As Integer)
+        Me.item_number = item_number
+        Me.price = price
+        Me.stock = stock
+    End Sub
+
+    'overrides .tostring for this class only
+    Public Overrides Function ToString() As String
+        Dim item_string As String = ("item number = " & item_number & " | price = " & price & " | stock = " & stock)
+
+        Return item_string
+    End Function
+
 
 End Class
--- InventoryMGMT/MainWindow.xaml.vb
+++ InventoryMGMT/MainWindow.xaml.vb
@@ -1,125 +1,65 @@
 ﻿Class MainWindow 
-    Dim items_class As New ITEMS
-    Dim item_class As New ITEM_CLASS
-
+    Dim items_list As New List(Of ITEM_CLASS)
+    Dim search_list As New List(Of ITEM_CLASS)
+    Dim purchase_list As New List(Of ITEM_CLASS)
 
     Private Sub search_bttn_tran_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles search_bttn_tran.Click
+        Dim found_index As Integer = search(search_txtbox_tran.Text, items_list)
 
-        Dim items_list As New List(Of ITEM_CLASS)
+        If found_index <> -1 Then
+            search_list.Clear()
 
-        'kind of works. Doesnt show in lstbox but items_list(0).item_number is grabbibg the right number 
-        listbox_item_tran.Items.Add(items_list(0).item_number)
-        listbox_item_tran.Items.Refresh()
+            search_list.Add(items_list(found_index))
+            listbox_tran.ItemsSource = search_list
+            listbox_tran.Items.Refresh()
 
-        'Dim item_numbers As List(Of String) = items_class.item_number
-        'Dim prices As List(Of Double) = items_class.price
-        'Dim in_stock As List(Of Integer) = items_class.stock
-
-
-        'listbox_item_tran.Items.Clear()
-        'listbox_price_tran.Items.Clear()
-        'listbox_stock_tran.Items.Clear()
-
-        'For x = 0 To item_numbers.Count - 1
-
-
-        '    'If items.item_number.Contains(search_txtbox_tran.Text) Then
-        '    If items_class.item_number(x) = search_txtbox_tran.Text Then
-
-        '        listbox_item_tran.Items.Add(item_numbers(x))
-        '        listbox_price_tran.Items.Add(prices(x))
-        '        listbox_stock_tran.Items.Add(in_stock(x))
-
-
-        '    End If
-
-        '    listbox_item_tran.Items.Refresh()
-        'Next
+        Else
+            listbox_tran.Items.Add("Not found")
+        End If
 
     End Sub
+
+
+    Public Function search(ByVal item_number As String, ByVal list As List(Of ITEM_CLASS))
+
+        For x As Integer = 0 To list.Count - 1
+            If item_number = list(x).item_number Then
+                Return x
+
+            End If
+        Next
+        'once a function returns it doesnt continue down the function (if it returns X it wont make it to -1)
+        Return -1
+
+    End Function
 
 
     Public Sub Window_Loaded(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles MyBase.Loaded
 
-        Dim items_list As New List(Of ITEM_CLASS)
+        Dim current_item1 As New ITEM_CLASS("1111", 12.99, 4)
+        items_list.Add(current_item1)
 
-        'MUCH BETTER THAN BEFORE!
-        'CAN NOW STORE MULTIPLE LISTS IN ONE VAR!!
-
-
-        'listbox_item_tran.Items.Add(item_class.item_number(0))
-        ' somehting like this for adding to txt box
-        'everything is a mess right now but this is whats important!!
-
-        Dim current_item As New ITEM_CLASS
+        Dim current_item2 As New ITEM_CLASS("2222", 3.99, 20)
+        items_list.Add(current_item2)
 
 
-        For x = 0 To 1
-            current_item.item_number = "1111"
-            current_item.price = 12.99
-            current_item.stock = 4
+        listbox_inv.ItemsSource = items_list
 
-            items_list.Add(current_item)
-
-            current_item.item_number = "1112"
-            current_item.price = 3.49
-            current_item.stock = 20
-
-            items_list.Add(current_item)
+    End Sub
 
 
+    Private Sub add_bttn_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles add_bttn.Click
+        Dim total_price_current As Double = 0
+
+        purchase_list.Add(search_list(0))
+        listbox_purchase.ItemsSource = purchase_list
+        listbox_purchase.Items.Refresh()
+
+        For current_item_index = 0 To purchase_list.Count - 1
+            total_price_current += purchase_list(current_item_index).price
         Next
 
-        
-
-        ''Load default items
-        'items_class.item_number.Add("1111")
-        'items_class.price.Add(9.99)
-        'items_class.stock.Add(4)
-
-        'items_class.item_number.Add("1112")
-        'items_class.price.Add(3.49)
-        'items_class.stock.Add(20)
-
-        'items_class.item_number.Add("1113")
-        'items_class.price.Add(1.99)
-        'items_class.stock.Add(10)
-
-        'items_class.item_number.Add("1114")
-        'items_class.price.Add(20.0)
-        'items_class.stock.Add(15)
-
-        'Dim item_numbers As List(Of String) = items_class.item_number
-        'Dim prices As List(Of Double) = items_class.price
-        'Dim in_stock As List(Of Integer) = items_class.stock
-
-
-        'For x = 0 To item_numbers.Count - 1
-        '    listbox_item_inv.Items.Add(item_numbers(x))
-        '    listbox_price_inv.Items.Add(prices(x))
-        '    listbox_stock_inv.Items.Add(in_stock(x))
-        'Next
-
+        label_price_currect.Content = total_price_current
 
     End Sub
-
-    Private Sub search_bttn_inv_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles search_bttn_inv.Click
-        'Dim item_numbers As List(Of String) = items_class.item_number
-        'Dim prices As List(Of Double) = items_class.price
-        'Dim in_stock As List(Of Integer) = items_class.stock
-
-
-        'does not work yet
-        'search inv page to highlight .contains
-        '    For x = 0 To item_numbers.Count - 1
-        '        If items_class.item_number.Contains(search_txtbox_inv.Text) Then
-
-        '            listbox_item_inv.Items.Add(item_numbers(x))
-        '            listbox_price_inv.Items.Add(prices(x))
-        '            listbox_stock_inv.Items.Add(in_stock(x))
-        '        End If
-        '    Next
-    End Sub
-
-
 End Class
