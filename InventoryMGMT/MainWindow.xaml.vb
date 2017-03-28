317b1efb0c20b1a78d131cb17f642cacf698f019
Sepohobbs <seppohobbs@gmail.com>
2017-03-28 11:39:37 -0500

    3/27

--- InventoryMGMT/MainWindow.xaml.vb
+++ InventoryMGMT/MainWindow.xaml.vb
@@ -3,6 +3,8 @@
     Dim search_list As New List(Of ITEM_CLASS)
     Dim purchase_list As New List(Of ITEM_CLASS)
 
+    Dim temp_stock As New List(Of Integer)
+
     Private Sub search_bttn_tran_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles search_bttn_tran.Click
         Dim found_index As Integer = search(search_txtbox_tran.Text, items_list)
 
@@ -42,6 +44,14 @@
         Dim current_item2 As New ITEM_CLASS("2222", 3.99, 20)
         items_list.Add(current_item2)
 
+        Dim current_item3 As New ITEM_CLASS("1122", 5.99, 10)
+        items_list.Add(current_item3)
+
+        Dim current_item4 As New ITEM_CLASS("2233", 3.5, 11)
+        items_list.Add(current_item4)
+
+        Dim current_item5 As New ITEM_CLASS("1234", 4.25, 22)
+        items_list.Add(current_item5)
 
         listbox_inv.ItemsSource = items_list
 
@@ -57,9 +67,47 @@
 
         For current_item_index = 0 To purchase_list.Count - 1
             total_price_current += purchase_list(current_item_index).price
+
         Next
 
+        'adds a temp stock to keep from going negative
+        temp_stock.Add(purchase_list(purchase_list.Count - 1).stock)
+
         label_price_currect.Content = total_price_current
+
+    End Sub
+
+   
+    Private Sub search_txtbox_inv_TextChanged(ByVal sender As System.Object, ByVal e As System.Windows.Controls.TextChangedEventArgs) Handles search_txtbox_inv.TextChanged
+        Dim search_results As New List(Of ITEM_CLASS)
+
+        For x As Integer = 0 To items_list.Count - 1
+            If items_list(x).item_number.StartsWith(search_txtbox_inv.Text) Then
+                search_results.Add(items_list(x))
+
+            End If
+
+        Next
+        listbox_inv.ItemsSource = search_results
+        listbox_inv.Items.Refresh()
+    End Sub
+
+    Private Sub purchase_bttn_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles purchase_bttn.Click
+        Dim total_price_final As Double = 0
+
+
+        For current_item_index = 0 To purchase_list.Count - 1
+            total_price_final += purchase_list(current_item_index).price
+        Next
+
+
+        For current_item_index = 0 To purchase_list.Count - 1
+            purchase_list(current_item_index).stock = purchase_list(current_item_index).stock - 1
+
+        Next
+        listbox_inv.Items.Refresh()
+
+        MessageBox.Show(total_price_final)
 
     End Sub
 End Class
